94a49279fdc5f8d33e1f243cd20917523980e7b8
JustAHuman-xD <imjustahuman.xd@gmail.com>
2026-02-18 18:15:15 -0600

    update assembly table tools

--- src/main/java/io/github/pylonmc/pylon/content/assembling/RedstoneSolderingIron.java
+++ src/main/java/io/github/pylonmc/pylon/content/assembling/RedstoneSolderingIron.java
@@ -6,6 +6,7 @@ import io.github.pylonmc.pylon.PylonKeys;
 import io.github.pylonmc.rebar.block.BlockStorage;
 import io.github.pylonmc.rebar.block.RebarBlock;
 import io.github.pylonmc.rebar.config.adapter.ConfigAdapter;
+import io.github.pylonmc.rebar.event.api.annotation.MultiHandler;
 import io.github.pylonmc.rebar.i18n.RebarArgument;
 import io.github.pylonmc.rebar.item.RebarItem;
 import io.github.pylonmc.rebar.item.base.RebarBlockInteractor;
@@ -13,7 +14,9 @@ import io.github.pylonmc.rebar.util.gui.unit.UnitFormat;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -37,29 +40,29 @@ public class RedstoneSolderingIron extends RebarItem implements RebarBlockIntera
         );
     }
 
-    @Override
-    public void onUsedToClickBlock(@NotNull PlayerInteractEvent event) {
-        event.setUseInteractedBlock(Event.Result.DENY);
-
-        if (!event.getAction().isLeftClick()) {
+    @Override @MultiHandler(priorities = { EventPriority.NORMAL, EventPriority.MONITOR })
+    public void onUsedToClickBlock(@NotNull PlayerInteractEvent event, @NotNull EventPriority priority) {
+        if (!event.getAction().isLeftClick()
+            || event.getHand() != EquipmentSlot.HAND
+            || event.useItemInHand() == Event.Result.DENY
+            || !(BlockStorage.get(event.getClickedBlock()) instanceof AssemblyTable assemblyTable)) {
             return;
         }
 
-        Block block = event.getClickedBlock();
-        Preconditions.checkState(block != null);
+        if (priority == EventPriority.NORMAL) {
+            event.setUseInteractedBlock(Event.Result.DENY);
+            return;
+        }
 
-        RebarBlock rebarBlock = BlockStorage.get(block);
-        if (rebarBlock instanceof AssemblyTable assemblyTable) {
-            if (assemblyTable.useTool(toolType, event.getPlayer())) {
-                getStack().damage(1, event.getPlayer());
-                event.getPlayer().setCooldown(PylonKeys.REDSTONE_SOLDERING_IRON, cooldownTicks);
-                new ParticleBuilder(Particle.SMOKE)
-                        .location(assemblyTable.getWorkspaceCenter())
-                        .extra(0)
-                        .count(10)
-                        .offset(assemblyTable.scale / 4, 0, assemblyTable.scale / 4)
-                        .spawn();
-            }
+        if (assemblyTable.useTool(toolType, event.getPlayer())) {
+            getStack().damage(1, event.getPlayer());
+            event.getPlayer().setCooldown(PylonKeys.REDSTONE_SOLDERING_IRON, cooldownTicks);
+            new ParticleBuilder(Particle.SMOKE)
+                    .location(assemblyTable.getWorkspaceCenter())
+                    .extra(0)
+                    .count(10)
+                    .offset(assemblyTable.scale / 4, 0, assemblyTable.scale / 4)
+                    .spawn();
         }
     }
 }
