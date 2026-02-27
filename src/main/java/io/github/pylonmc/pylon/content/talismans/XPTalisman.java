fc4f7b32ff5cf24f586a72275c7ce199e58df997
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-02-27 10:49:59 -0500

    add xp talisman

---
+++ src/main/java/io/github/pylonmc/pylon/content/talismans/XPTalisman.java
@@ -0,0 +1,57 @@
+package io.github.pylonmc.pylon.content.talismans;
+
+import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
+import io.github.pylonmc.rebar.config.adapter.ConfigAdapter;
+import io.github.pylonmc.rebar.i18n.RebarArgument;
+import io.github.pylonmc.rebar.util.gui.unit.UnitFormat;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static io.github.pylonmc.pylon.util.PylonUtils.pylonKey;
+
+public class XPTalisman extends Talisman {
+    public static final NamespacedKey XP_TALISMAN_KEY = pylonKey("xp_talisman");
+    public static final NamespacedKey XP_MULTIPLIER_KEY = pylonKey("xp_talisman_multiplier");
+    public final float xpMultiplier = getSettings().getOrThrow("xp-multiplier", ConfigAdapter.FLOAT);
+
+    public XPTalisman(@NotNull ItemStack stack) {
+        super(stack);
+    }
+
+    @Override
+    public NamespacedKey getTalismanKey() {
+        return XP_TALISMAN_KEY;
+    }
+
+    @Override
+    public void applyEffect(@NotNull Player player) {
+        super.applyEffect(player);
+        player.getPersistentDataContainer().set(XP_MULTIPLIER_KEY, PersistentDataType.FLOAT, xpMultiplier);
+    }
+
+    @Override
+    public @NotNull List<@NotNull RebarArgument> getPlaceholders() {
+        return List.of(RebarArgument.of("xp-boost", UnitFormat.PERCENT.format(xpMultiplier - 1).decimalPlaces(0)));
+    }
+
+    public static class XPTalismanListener implements Listener {
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void onPlayerGainXP(PlayerPickupExperienceEvent event){
+            Float xpMultiplier = event.getPlayer().getPersistentDataContainer().get(XP_MULTIPLIER_KEY, PersistentDataType.FLOAT);
+            if(xpMultiplier == null){
+                return;
+            }
+            event.getExperienceOrb().setExperience(
+                    Math.round(event.getExperienceOrb().getExperience() * xpMultiplier)
+            );
+        }
+    }
+}
