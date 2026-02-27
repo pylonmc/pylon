bdb6059d6cd78339504b95e56f2f9f16f5c3265d
balugaq <129668183+balugaq@users.noreply.github.com>
2026-02-27 15:38:31 +0800

    merge upstream

--- src/main/java/io/github/pylonmc/pylon/content/talismans/FlightRing.java
+++ src/main/java/io/github/pylonmc/pylon/content/talismans/FlightRing.java
@@ -7,7 +7,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public final class FlightRing extends Talisman {
+    private static final Set<UUID> cannotFlyPlayers = new HashSet<>();
     public FlightRing(@NotNull ItemStack stack) {
         super(stack);
     }
@@ -20,7 +25,10 @@ public final class FlightRing extends Talisman {
     @Override
     public void applyEffect(@NotNull Player player) {
         super.applyEffect(player);
-        player.setAllowFlight(true);
+        if (!player.getAllowFlight()) {
+            cannotFlyPlayers.add(player.getUniqueId());
+            player.setAllowFlight(true);
+        }
         player.setFlying(true);
     }
 
@@ -31,7 +39,10 @@ public final class FlightRing extends Talisman {
         var gm = player.getGameMode();
         if (gm != GameMode.CREATIVE && gm != GameMode.SPECTATOR) {
             player.setFlying(false);
-            player.setAllowFlight(false);
+            if (cannotFlyPlayers.contains(player.getUniqueId())) {
+                player.setAllowFlight(false);
+                cannotFlyPlayers.remove(player.getUniqueId());
+            }
         }
     }
 }
