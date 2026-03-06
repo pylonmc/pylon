dd5231e3863335a221e2c36069b2637fca4f5c2e
Intybyte <vaan.testwork@gmail.com>
2026-03-06 12:21:51 +0100

    Add Collimator recipe

--- src/main/java/io/github/pylonmc/pylon/PylonRecipes.java
+++ src/main/java/io/github/pylonmc/pylon/PylonRecipes.java
@@ -1,6 +1,19 @@
 package io.github.pylonmc.pylon;
 
 import io.github.pylonmc.pylon.recipes.*;
+import io.github.pylonmc.rebar.config.Config;
+import io.github.pylonmc.rebar.config.Settings;
+import io.github.pylonmc.rebar.config.adapter.ConfigAdapter;
+import io.github.pylonmc.rebar.guide.button.FluidButton;
+import io.github.pylonmc.rebar.guide.button.ItemButton;
+import io.github.pylonmc.rebar.recipe.FluidOrItem;
+import io.github.pylonmc.rebar.recipe.RecipeInput;
+import io.github.pylonmc.rebar.util.gui.GuiItems;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import xyz.xenondevs.invui.gui.Gui;
+
+import java.util.List;
 
 
 public class PylonRecipes {
@@ -28,5 +41,32 @@ public class PylonRecipes {
         PitKilnRecipe.RECIPE_TYPE.register();
         StrainingRecipe.RECIPE_TYPE.register();
         TableSawRecipe.RECIPE_TYPE.register();
+
+        //hardcoded
+        initCollimator();
+    }
+
+    private static void initCollimator() {
+        NamespacedKey key = PylonKeys.COLLIMATOR;
+        RecipeInput.Fluid input = RecipeInput.of(PylonFluids.OBSCYRA, Settings.get(key).getOrThrow("obscyra-per-cohesive-unit", ConfigAdapter.INTEGER));
+        FluidOrItem output = FluidOrItem.of(PylonItems.COHESIVE_UNIT);
+        new SingleRecipe(
+                key,
+                input,
+                output,
+                Gui.builder()
+                        .setStructure(
+                                "# # # # # # # # #",
+                                "# # # # # # # # #",
+                                "# i # # x # # o #",
+                                "# # # # # # # # #",
+                                "# # # # # # # # #"
+                        )
+                        .addIngredient('#', GuiItems.backgroundBlack())
+                        .addIngredient('i', new FluidButton(input.amountMillibuckets(), PylonFluids.OBSCYRA))
+                        .addIngredient('x', ItemButton.from(PylonItems.COLLIMATOR))
+                        .addIngredient('o', ItemButton.from(PylonItems.COHESIVE_UNIT))
+                        ::build
+        ).register();
     }
 }
