e471058791b359de68bb47e90b0fa776c4409a03
balugaq <129668183+balugaq@users.noreply.github.com>
2026-03-06 22:16:39 +0800

    Update HuntingTalisman.java

--- src/main/java/io/github/pylonmc/pylon/content/talismans/HuntingTalisman.java
+++ src/main/java/io/github/pylonmc/pylon/content/talismans/HuntingTalisman.java
@@ -4,17 +4,26 @@ import io.github.pylonmc.pylon.util.PylonUtils;
 import io.github.pylonmc.rebar.config.adapter.ConfigAdapter;
 import io.github.pylonmc.rebar.i18n.RebarArgument;
 import io.github.pylonmc.rebar.util.gui.unit.UnitFormat;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.ChestedHorse;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Ravager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.LlamaInventory;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -67,15 +76,65 @@ public class HuntingTalisman extends Talisman {
             }
             @SuppressWarnings("DataFlowIssue")
             double chanceForExtraItem = player.getPersistentDataContainer().get(HUNTING_TALISMAN_BONUS_KEY, PersistentDataType.DOUBLE);
-            for (ItemStack drop : event.getDrops()) {
+            for (ItemStack drop : getExtraDrops(event.getEntity(), event.getDrops())) {
                 if (drop.getItemMeta().hasRarity() && drop.getItemMeta().getRarity().ordinal() >= ItemRarity.RARE.ordinal()) {
                     continue;
                 }
                 if (ThreadLocalRandom.current().nextDouble() > chanceForExtraItem) {
                     continue;
                 }
-                drop.setAmount(drop.getAmount() + 1);
+                event.getDrops().add(drop.asOne());
             }
+        }
+
+        private List<ItemStack> getExtraDrops(LivingEntity entity, List<ItemStack> drops) {
+            if (entity.getType() == EntityType.PLAYER || entity.getType() == EntityType.ARMOR_STAND) {
+                return List.of();
+            }
+
+            List<ItemStack> items = new ArrayList<>(drops);
+
+            // No item duplications from horse-like entities' chest
+            if (entity instanceof ChestedHorse horse && horse.isCarryingChest()) {
+                // The carrying chest is not included in getStorageContents()
+                items.remove(ItemStack.of(Material.CHEST));
+
+                for (ItemStack stack : horse.getInventory().getStorageContents()) {
+                    items.remove(stack);
+                }
+            }
+
+            if (!(entity instanceof Ravager)) {
+                // Only ravager drops saddle when killed
+                items.remove(ItemStack.of(Material.SADDLE));
+            }
+
+            if (entity instanceof AbstractHorse horse) {
+                // saddle has been removed above
+                var inventory = horse.getInventory();
+
+                if (inventory instanceof LlamaInventory llamaInventory) {
+                    items.remove(llamaInventory.getDecor());
+                } else {
+                    ItemStack horseArmor = inventory.getItem(1);
+                    if (horseArmor != null) {
+                        // Horse armor
+                        items.remove(horseArmor);
+                    }
+                }
+            }
+
+            EntityEquipment equipment = entity.getEquipment();
+            if (equipment != null) {
+                for (EquipmentSlot slot : EquipmentSlot.values()) {
+                    if (entity.canUseEquipmentSlot(slot)) {
+                        // see EquipmentSlot
+                        items.remove(equipment.getItem(slot));
+                    }
+                }
+            }
+
+            return items;
         }
     }
 }
