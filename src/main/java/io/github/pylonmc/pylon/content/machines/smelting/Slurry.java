88ad58aa1487fd0dfd12882dc65d7fa127ffd215
Intybyte <vaan.testwork@gmail.com>
2026-03-17 12:00:36 +0100

    Buff steel production

--- src/main/java/io/github/pylonmc/pylon/content/machines/smelting/Slurry.java
+++ src/main/java/io/github/pylonmc/pylon/content/machines/smelting/Slurry.java
@@ -9,6 +9,7 @@ import io.github.pylonmc.rebar.recipe.FluidOrItem;
 import io.github.pylonmc.rebar.recipe.RecipeInput;
 import io.github.pylonmc.rebar.registry.RebarRegistry;
 import io.github.pylonmc.rebar.registry.RegistryHandler;
+import lombok.Builder;
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
@@ -22,14 +23,32 @@ public class Slurry extends RebarFluid implements RegistryHandler {
 
     @Getter
     private final ItemStack slurryStack;
+
+    @Getter
+    private final int amount;
+
+
+    public Slurry(@NotNull NamespacedKey key, @NotNull Component name, @NotNull ItemStack slurryStack, @NotNull List<RebarFluidTag> tags, int amount) {
+        super(key, name, Material.LIGHT_GRAY_CONCRETE, tags);
+        this.amount = amount;
+        this.slurryStack = slurryStack;
+    }
     
     public Slurry(@NotNull NamespacedKey key, @NotNull Component name, @NotNull ItemStack slurryStack, @NotNull List<RebarFluidTag> tags) {
         super(key, name, Material.LIGHT_GRAY_CONCRETE, tags);
+        this.amount = 1000;
         this.slurryStack = slurryStack;
     }
 
     public Slurry(@NotNull NamespacedKey key, @NotNull ItemStack slurryStack, @NotNull RebarFluidTag @NotNull ... tags) {
         super(key, Material.LIGHT_GRAY_CONCRETE, tags);
+        this.amount = 1000;
+        this.slurryStack = slurryStack;
+    }
+
+    public Slurry(@NotNull NamespacedKey key, @NotNull ItemStack slurryStack, int amount, @NotNull RebarFluidTag @NotNull ... tags) {
+        super(key, Material.LIGHT_GRAY_CONCRETE, tags);
+        this.amount = amount;
         this.slurryStack = slurryStack;
     }
 
@@ -38,13 +57,13 @@ public class Slurry extends RebarFluid implements RegistryHandler {
         MixingPotRecipe.RECIPE_TYPE.addRecipe(new MixingPotRecipe(
                 getKey(),
                 List.of(RecipeInput.of(slurryStack)),
-                RecipeInput.of(PylonFluids.SLURRY, 1000),
-                FluidOrItem.of(this, 1000),
+                RecipeInput.of(PylonFluids.SLURRY, amount),
+                FluidOrItem.of(this, amount),
                 false
         ));
         StrainingRecipe.RECIPE_TYPE.addRecipe(new StrainingRecipe(
                 getKey(),
-                RecipeInput.of(this, 1000),
+                RecipeInput.of(this, amount),
                 PylonFluids.SLURRY,
                 slurryStack
         ));
