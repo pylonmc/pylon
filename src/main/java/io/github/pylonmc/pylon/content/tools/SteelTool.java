a6551dc9a04ca78376afff981cfe4f34b554353e
JustAHuman-xD <imjustahuman.xd@gmail.com>
2026-05-09 20:25:03 -0500

    make tools and armor repairable

--- src/main/java/io/github/pylonmc/pylon/content/tools/SteelTool.java
+++ src/main/java/io/github/pylonmc/pylon/content/tools/SteelTool.java
@@ -1,29 +1,21 @@
-package io.github.pylonmc.pylon.content.armor;
+package io.github.pylonmc.pylon.content.tools;
 
 import io.github.pylonmc.pylon.PylonKeys;
-import io.github.pylonmc.pylon.util.PylonUtils;
 import io.github.pylonmc.rebar.item.RebarItem;
-import io.github.pylonmc.rebar.item.base.RebarArmor;
 import io.github.pylonmc.rebar.item.base.RebarRepairable;
-import net.kyori.adventure.key.Key;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class SteelArmor extends RebarItem implements RebarArmor, RebarRepairable {
-    public SteelArmor(@NotNull ItemStack stack) {
+public class SteelTool extends RebarItem implements RebarRepairable {
+    public SteelTool(@NotNull ItemStack stack) {
         super(stack);
     }
 
     @Override
     public @NotNull List<NamespacedKey> getRepairItems() {
         return List.of(PylonKeys.STEEL_INGOT);
-    }
-
-    @Override
-    public @NotNull Key getEquipmentType() {
-        return PylonUtils.pylonKey("steel");
     }
 }
