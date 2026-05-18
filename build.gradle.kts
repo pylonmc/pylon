df15c1c9fd13f5fb05ae9f3a6432a31d14ff3cf8
JustAHuman-xD <imjustahuman.xd@gmail.com>
2026-05-18 09:26:00 -0500

    bump run-paper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     idea
     id("com.gradleup.shadow") version "9.0.0"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
     id("io.freefair.lombok") version "9.5.0"
     `maven-publish`
     signing
