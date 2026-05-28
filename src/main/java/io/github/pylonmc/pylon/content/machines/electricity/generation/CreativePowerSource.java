927213a19564c400c459b95b7eb850eb48ea0fe3
Seggan <seggan21@gmail.com>
2026-05-28 15:09:07 -0400

    New and improved creative power source

--- src/main/java/io/github/pylonmc/pylon/content/machines/electricity/generation/CreativePowerSource.java
+++ src/main/java/io/github/pylonmc/pylon/content/machines/electricity/generation/CreativePowerSource.java
@@ -1,35 +1,38 @@
 package io.github.pylonmc.pylon.content.machines.electricity.generation;
 
 import io.github.pylonmc.rebar.block.RebarBlock;
-import io.github.pylonmc.rebar.block.base.RebarInteractBlock;
 import io.github.pylonmc.rebar.block.base.RebarSimpleElectricBlock;
+import io.github.pylonmc.rebar.block.base.RebarTickingBlock;
 import io.github.pylonmc.rebar.block.context.BlockCreateContext;
-import io.github.pylonmc.rebar.i18n.RebarArgument;
-import io.github.pylonmc.rebar.util.PlayerInput;
+import io.github.pylonmc.rebar.entity.display.BlockDisplayBuilder;
+import io.github.pylonmc.rebar.entity.display.transform.TransformBuilder;
 import io.github.pylonmc.rebar.util.RebarUtils;
-import io.github.pylonmc.rebar.util.gui.unit.UnitFormat;
-import io.github.pylonmc.rebar.waila.WailaDisplay;
-import net.kyori.adventure.text.Component;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.entity.BlockDisplay;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.jetbrains.annotations.NotNull;
-import org.jspecify.annotations.NonNull;
+import org.joml.Vector3f;
 
-public final class CreativePowerSource extends RebarBlock implements
-        RebarSimpleElectricBlock,
-        RebarInteractBlock {
+public final class CreativePowerSource extends RebarBlock implements RebarSimpleElectricBlock, RebarTickingBlock {
+
+    private static final float BLACK_HOLE_SIZE = 0.25f;
 
     @SuppressWarnings("unused")
     public CreativePowerSource(@NotNull Block block, @NotNull BlockCreateContext context) {
         super(block, context);
+        setTickInterval(1);
         for (BlockFace face : RebarUtils.IMMEDIATE_FACES) {
             createSimpleElectricPort(NodeType.PRODUCER, face);
         }
+        addEntity("black_hole", new BlockDisplayBuilder()
+                .material(Material.END_GATEWAY)
+                .transformation(new TransformBuilder().scale(BLACK_HOLE_SIZE))
+                .build(block.getLocation().toCenterLocation()));
+        setPowerProduced(1e21);
     }
 
     @SuppressWarnings({"unused"})
@@ -37,31 +40,23 @@ public final class CreativePowerSource extends RebarBlock implements
         super(block, pdc);
     }
 
-    @Override
-    public void onInteract(@NotNull PlayerInteractEvent event, @NotNull EventPriority priority) {
-        Player player = event.getPlayer();
-        if (player.isSneaking() || !event.getAction().isRightClick() || event.getHand() != EquipmentSlot.HAND) return;
-        player.sendMessage(Component.translatable("pylon.message.creative-power-source.set-power"));
-        PlayerInput.requestInput(player).thenAccept(input -> {
-            if (input == null) return;
-            try {
-                double newValue = Double.parseDouble(input);
-                setPowerProduced(Math.max(0, newValue));
-            } catch (NumberFormatException e) {
-                player.sendMessage(Component.translatable("pylon.message.invalid-input.double"));
-            }
-        });
-    }
+    private int animationTicksLeft = 0;
 
     @Override
-    public @NonNull WailaDisplay getWaila(@NotNull Player player) {
-        return new WailaDisplay(getDefaultWailaTranslationKey().arguments(
-                RebarArgument.of("power", UnitFormat.WATTS.format(getPowerProduced())
-                        .ignoreCommonlyUnusedPrefixes()
-                        .abbreviate(true)
-                        .autoSelectPrefix()
-                        .decimalPlaces(2)
-                )
-        ));
+    public void tick() {
+        if (animationTicksLeft-- > 0) return;
+        Random random = ThreadLocalRandom.current();
+        BlockDisplay blackHole = getHeldEntityOrThrow(BlockDisplay.class, "black_hole");
+
+        int ticks = random.nextInt(1, 20);
+        blackHole.setInterpolationDelay(0);
+        blackHole.setInterpolationDuration(ticks);
+        animationTicksLeft = ticks;
+
+        Vector3f randomAxis = new Vector3f(random.nextFloat(), random.nextFloat(), random.nextFloat()).normalize();
+        blackHole.setTransformationMatrix(new TransformBuilder()
+                .rotate(randomAxis, (float) (2 * Math.PI) * random.nextFloat())
+                .scale(BLACK_HOLE_SIZE * random.nextFloat() + BLACK_HOLE_SIZE / 2)
+                .buildForBlockDisplay());
     }
 }
