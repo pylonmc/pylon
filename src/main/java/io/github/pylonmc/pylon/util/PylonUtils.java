c79e0081b49e95f93e5b710e802242ba866e8dcd
balugaq <129668183+balugaq@users.noreply.github.com>
2026-06-09 15:29:27 +0800

    use fluid data

--- src/main/java/io/github/pylonmc/pylon/util/PylonUtils.java
+++ src/main/java/io/github/pylonmc/pylon/util/PylonUtils.java
@@ -152,8 +152,7 @@ public class PylonUtils {
 
     public boolean shouldBreakBlockUsingTool(@NotNull Block block, @NotNull ItemStack tool) {
         return !block.getType().isAir()
-                && block.getType() != Material.LAVA
-                && block.getType() != Material.WATER
+                && block.getWorld().getFluidData(block.getLocation()).getFluidType() == Fluid.EMPTY
                 && !(block.getState() instanceof BlockInventoryHolder)
                 && !BlockStorage.isRebarBlock(block)
                 && block.getType().getHardness() >= 0
