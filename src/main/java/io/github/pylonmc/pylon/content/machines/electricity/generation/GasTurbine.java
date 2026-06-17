dba48c383450b899f3ef0670ef28a3fc1d988f17
Seggan <seggan21@gmail.com>
2026-06-16 22:53:32 -0400

    Nuuk `Vectors.kt`

--- src/main/java/io/github/pylonmc/pylon/content/machines/electricity/generation/GasTurbine.java
+++ src/main/java/io/github/pylonmc/pylon/content/machines/electricity/generation/GasTurbine.java
@@ -16,7 +16,6 @@ import io.github.pylonmc.rebar.entity.display.transform.TransformUtil;
 import io.github.pylonmc.rebar.fluid.RebarFluid;
 import io.github.pylonmc.rebar.i18n.RebarArgument;
 import io.github.pylonmc.rebar.item.builder.ItemStackBuilder;
-import io.github.pylonmc.rebar.util.Vector3fs;
 import io.github.pylonmc.rebar.util.gui.unit.UnitFormat;
 import io.github.pylonmc.rebar.waila.WailaDisplay;
 import java.util.ArrayList;
@@ -114,7 +113,7 @@ public class GasTurbine extends RebarBlock implements
             }
         }
 
-        Vector3f acrossDirection = new Vector3f(direction).cross(Vector3fs.positiveY()).normalize();
+        Vector3f acrossDirection = new Vector3f(direction).cross(new Vector3f(0, 1, 0)).normalize();
         Vector3f veryBack = new Vector3f(direction).mul(-1.5f).sub(0, 0.5f, 0).sub(new Vector3f(acrossDirection).mul(0.5f));
         int particles = (int) Math.ceil(Math.log(inputAmount));
         for (int i = 0; i < particles; i++) {
