4bcb305d0ec188ad9d801cb5ebc8acddff4b3206
Seggan <seggan21@gmail.com>
2026-07-03 14:20:51 -0400

    Make wire unplacable

--- src/main/java/io/github/pylonmc/pylon/content/tools/PylonWire.java
+++ src/main/java/io/github/pylonmc/pylon/content/tools/PylonWire.java
@@ -3,14 +3,17 @@ package io.github.pylonmc.pylon.content.tools;
 import io.github.pylonmc.rebar.config.adapter.ConfigAdapter;
 import io.github.pylonmc.rebar.i18n.RebarArgument;
 import io.github.pylonmc.rebar.item.RebarItem;
+import io.github.pylonmc.rebar.item.interfaces.InteractRebarItemHandler;
 import io.github.pylonmc.rebar.item.interfaces.WireRebarItem;
 import io.github.pylonmc.rebar.util.gui.unit.UnitFormat;
 import java.util.List;
 import org.bukkit.Material;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class PylonWire extends RebarItem implements WireRebarItem {
+public class PylonWire extends RebarItem implements WireRebarItem, InteractRebarItemHandler {
 
     private final double maxPower = getSettingOrThrow("max-power", ConfigAdapter.DOUBLE);
 
@@ -31,5 +34,10 @@ public class PylonWire extends RebarItem implements WireRebarItem {
     @Override
     public @NotNull Material getDisplayMaterial() {
         return Material.COPPER_BLOCK;
+    }
+
+    @Override
+    public void onInteract(@NotNull PlayerInteractEvent event, @NotNull EventPriority priority) {
+        event.setCancelled(true);
     }
 }
