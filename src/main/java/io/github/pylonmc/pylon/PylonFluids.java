0b6d7fd7c110e869a071e3670d7f626efb24dc5b
balugaq <129668183+balugaq@users.noreply.github.com>
2026-07-10 14:00:27 +0800

    Update PylonFluids.java

--- src/main/java/io/github/pylonmc/pylon/PylonFluids.java
+++ src/main/java/io/github/pylonmc/pylon/PylonFluids.java
@@ -15,9 +15,6 @@ import io.github.pylonmc.pylon.content.machines.smelting.Slurry;
 import io.github.pylonmc.rebar.content.guide.RebarGuide;
 import io.github.pylonmc.rebar.fluid.RebarFluid;
 import io.github.pylonmc.rebar.fluid.tags.FluidTemperature;
-import io.github.pylonmc.rebar.recipe.IngredientCalculator;
-import org.jetbrains.annotations.NotNull;
-
 import io.github.pylonmc.rebar.recipe.ingredient.IngredientCalculator;
 
 public final class PylonFluids {
